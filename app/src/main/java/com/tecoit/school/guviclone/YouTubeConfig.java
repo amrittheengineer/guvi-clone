3626ca13c8fe388b13fb02a0be404623dcc354f0
Amrit <amrittheengineer@users.noreply.github.com>
2020-12-31 02:55:36 +0530

    Added YoutubeConfig file with APIKey.

---
+++ app/src/main/java/com/tecoit/school/guviclone/YouTubeConfig.java
@@ -0,0 +1,9 @@
+package com.tecoit.school.guviclone;
+
+public class YouTubeConfig {
+    private static final String API_KEY = "AIzaSyCyhfq5BjSSkT_V_x6wW40Can1GGz5K7qg";
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+}
